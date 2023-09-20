fed63c4edd329810aee420c909d6a7b4cb49170d
NISHANT SUMAN <132561602+ns-nishant89@users.noreply.github.com>
2023-09-20 09:05:26 +0530

    Time: 0 ms (100%), Space: 40.6 MB (44.82%) - LeetHub

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        
+        return bstFromPreorder(preorder, Integer.MAX_VALUE, new int[]{0});
+    }
+
+    public TreeNode bstFromPreorder(int[] preorder, int bound, int[] i){
+        
+        if(i[0] == preorder.length || preorder[i[0]] > bound){
+            return null;
+        }
+
+
+        TreeNode root = new TreeNode(preorder[i[0]++]);
+
+        root.left = bstFromPreorder(preorder, root.val, i);
+    
+        root.right = bstFromPreorder(preorder, bound, i);
+
+        return root;
+    }
+}
