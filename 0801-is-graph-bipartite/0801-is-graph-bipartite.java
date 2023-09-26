6c07e3a2dfad0eca4b2c0d6549e56b2a28c88c9e
NISHANT SUMAN <132561602+ns-nishant89@users.noreply.github.com>
2023-09-26 14:44:52 +0530

    Time: 6 ms (15.82%), Space: 44 MB (44.23%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,20 +1,97 @@
-
-        class Solution {
+/*                  <----:USING BFS:--->
+class Solution {
     public boolean isBipartite(int[][] graph) {
-        int colors[] = new int[graph.length];
-        for(int i = 0; i<graph.length; i++){
-            if(colors[i] == 0 && !dfs(i, graph, colors, true)){
-                return false;
+        int n=graph.length;
+        ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<graph[i].length;j++){
+                adj.get(i).add(graph[i][j]);
+            }
+        }
+        
+        int color[]=new int[n+1];
+        Arrays.fill(color,-1);
+
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                if(checkBipartite(i,adj,color)==false){
+                    return false;
+                }
             }
         }
         return true;
     }
-    private boolean dfs(int s, int[][] graph, int colors[], boolean red){
-        if(red) colors[s] = 1;
-        else colors[s] = 2;
-        for(int v : graph[s])
-            if(colors[v] == (red ? 1 : 2) || colors[v] == 0 && !dfs(v, graph, colors, !red)) return false;
+// colors a component 
+    public boolean checkBipartite(int i,ArrayList<ArrayList<Integer>>adj,int color[]){
+        Queue<Integer>q=new LinkedList<>();
+        q.add(i);
+        color[i]=0;
+
+        while(!q.isEmpty()){
+            int node=q.poll();
+            
+            for(int e:adj.get(node)){
+                if(color[e]==-1){// if the adjacent node is yet not colored
+                    color[e]=1-color[node];// you will give the opposite color of the node 
+                    q.add(e);
+                }
+                else if(color[e]==color[node]){// is the adjacent guy having the same color 
+                    return false;
+                }
+            }
+            
+        }
+        return true;
+
+    }
+}*/
+//                  <----:USING DFS:--->
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n=graph.length;
+        ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<graph[i].length;j++){
+                adj.get(i).add(graph[i][j]);
+            }
+        }
+        
+        int color[]=new int[n+1];
+        Arrays.fill(color,-1);
+
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                if(dfs(i,0,color,adj)==false){
+                    return false;
+                }
+            }
+        }
         return true;
     }
+    private boolean dfs(int node, int col, int color[], ArrayList<ArrayList<Integer>>adj) {   
+        color[node] = col; 
+        // traverse adjacent nodes
+        for(int it : adj.get(node)) {
+            // if uncoloured
+            if(color[it] == -1) {
+                if(dfs(it, 1 - col, color, adj) == false) return false; 
+            }
+            // if previously coloured and have the same colour
+            else if(color[it] == col) {
+                return false; 
+            }
+        }
+        
+        return true; 
+    }
 }
+        
     
