50ec050292e481c86bdee76e3129b3cabe86bc30
NISHANT SUMAN <132561602+ns-nishant89@users.noreply.github.com>
2023-09-26 14:39:08 +0530

    Time: 0 ms (100%), Space: 43.7 MB (82.63%) - LeetHub

---
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -0,0 +1,20 @@
+
+        class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int colors[] = new int[graph.length];
+        for(int i = 0; i<graph.length; i++){
+            if(colors[i] == 0 && !dfs(i, graph, colors, true)){
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean dfs(int s, int[][] graph, int colors[], boolean red){
+        if(red) colors[s] = 1;
+        else colors[s] = 2;
+        for(int v : graph[s])
+            if(colors[v] == (red ? 1 : 2) || colors[v] == 0 && !dfs(v, graph, colors, !red)) return false;
+        return true;
+    }
+}
+    
