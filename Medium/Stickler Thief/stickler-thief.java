5d9c4e44f47a1c606482c7404a8c650123635474
NISHANT SUMAN <132561602+ns-nishant89@users.noreply.github.com>
2023-11-08 09:37:40 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Stickler Thief/stickler-thief.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class GFG
+ {
+	public static void main (String[] args)
+	 {
+	  
+	  //taking input using Scanner class
+	  Scanner sc = new Scanner(System.in);
+	  
+	  //taking count of testcases
+	  int t = sc.nextInt();
+	  while(t-- > 0){
+	      
+	      //taking count of houses
+	      int n = sc.nextInt();
+	      int arr[] = new int[n];
+	      
+	      //inserting money present in 
+	      //each house in the array
+	      for(int i = 0; i<n; ++i)
+	           arr[i] = sc.nextInt();
+  	      
+  	      //calling method FindMaxSum() of class solve
+  	      System.out.println(new Solution().FindMaxSum(arr, n));
+	 }
+   }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum money the thief can get.
+    public int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        int prev =arr[0];
+        int prev2=0;
+        int curr;
+        for(int i=1;i<n;i++){
+            int pick = prev2+arr[i];
+            int nonpick = prev;
+            curr = Math.max(pick,nonpick);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
