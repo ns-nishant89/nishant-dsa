b0346999cd0fac28d6c7a58e8df045b7000dec1b
NISHANT SUMAN <132561602+ns-nishant89@users.noreply.github.com>
2024-02-07 16:35:09 +0530

    Time: 1 ms (98.33%) | Memory: 45.6 MB (29.79%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+     int [] pre  = new int [nums.length];
+     int sum=nums[0];
+     pre[0] = 0;
+     for(int i=1;i<nums.length;i++){
+         sum+=nums[i];
+         pre[i] = pre[i-1]+nums[i-1];
+     }   
+     for(int i=0;i<nums.length;i++){
+         if(pre[i]==sum-nums[i]-pre[i]){
+             return i;
+         }
+     }
+     return -1;
+    }
+}
