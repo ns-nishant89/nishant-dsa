cb4c3eac0915c61bbffcd17ecffd91bcee180695
NISHANT SUMAN <132561602+ns-nishant89@users.noreply.github.com>
2024-02-12 17:19:31 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximum number of partitions that can be sorted individually to make sorted/maximum-number-of-partitions-that-can-be-sorted-individually-to-make-sorted.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		     
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    Complete obj = new Complete();
+		    int ans = obj.maxPartitions(arr, sizeOfArray);
+		    System.out.println(ans);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int maxPartitions (int arr[], int n) {
+        int max=0;
+        int count =0;
+        for(int i=0;i<n;i++){
+            max = Math.max(arr[i],max);
+            if(i==max){
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    
+}
+
+
