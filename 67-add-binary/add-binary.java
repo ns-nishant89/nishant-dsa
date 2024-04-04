309e2f7f6c39f375d4e2b9ee8aff96a00109d4db
NISHANT SUMAN <132561602+ns-nishant89@users.noreply.github.com>
2024-04-04 12:29:38 +0530

    Time: 2 ms (53.24%) | Memory: 42.6 MB (22.00%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String addBinary(String a, String b) {
+       int alen = a.length();
+       int blen = b.length();
+       int i=0;
+       int carry=0;
+       StringBuilder ans = new StringBuilder();
+       while(i<alen||i<blen||carry!=0){
+        int x=0;
+        if(i<alen && a.charAt(alen-i-1)=='1'){
+            x=1;
+        }
+        int y=0;
+        if(i<blen&& b.charAt(blen-i-1)=='1'){
+           y=1;
+        }
+        ans.insert(0,(x+y+carry)%2);
+        carry = (x+y+carry)/2;
+        i+=1;
+       } 
+       return ans.toString();
+    }
+}
