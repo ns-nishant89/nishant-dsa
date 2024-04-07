34d5eb86e631f1d217ee8b15ff07528908095b7b
NISHANT SUMAN <132561602+ns-nishant89@users.noreply.github.com>
2024-04-07 10:07:35 +0530

    Time: 1 ms (81.28%) | Memory: 41.6 MB (50.68%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,32 +1,23 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-     Stack<Character> S1 = new Stack<>();
-     Stack<Character> T1 = new Stack<>();
-     StringBuilder str1 = new StringBuilder();
-     StringBuilder str2 = new StringBuilder();
-     for(int i=0;i<s.length();i++){
-        if(s.charAt(i)=='#' && !S1.isEmpty()){
-            S1.pop();
-        }else if(s.charAt(i)!='#'){
-            S1.push(s.charAt(i));
-        }
-     }
-     for(int i=0;i<t.length();i++){
-        if(t.charAt(i)=='#' && !T1.isEmpty()){
-            T1.pop();
-        }else if(t.charAt(i)!='#'){
-            T1.push(t.charAt(i));
-        }
-     }
-     while(!S1.isEmpty()){
-        str1.append(S1.peek());
-        S1.pop();
+      StringBuilder str1=compare(s);
+        StringBuilder str2=compare(t);
 
-     }   
-      while(!T1.isEmpty()){
-        str2.append(T1.peek());
-        T1.pop();
-     }   
-     return str1.toString().equals(str2.toString());
+        return str1.toString().equals(str2.toString());
+    }
+    
+    public StringBuilder compare(String s){
+        StringBuilder str = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='#'){
+                if(str.length()>0)
+                    str.deleteCharAt(str.length()-1);
+            } 
+            else
+                str.append(s.charAt(i));
+        }
+        
+        return str;
+       
     }
 }
