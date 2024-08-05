942588bb12c4e7d0be58f03d926eeb9c8421cf35
NISHANT SUMAN <132561602+ns-nishant89@users.noreply.github.com>
2024-08-05 15:10:22 +0530

    Time: 6 ms (91.98%) | Memory: 44.2 MB (51.23%) - LeetSync

---
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String,Integer>map = new HashMap<>();
+        for(String str:arr){
+            map.put(str,map.getOrDefault(str,0)+1);
+        }
+        for(String str:arr){
+            if(map.get(str)==1){
+                k--;
+            }
+            if(k==0){
+                return str;
+            }
+        }
+        return "";
+    }
+}
